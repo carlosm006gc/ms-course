b493a05acea3b55adffa7b3e180d55317d9e3ad4
carlosm006gc <104165201+carlosm006gc@users.noreply.github.com>
2023-02-09 11:24:36 -0300

    Hystrix fault tolerance

--- hr-worker/src/main/java/com/dev/hrworker/controllers/WorkerController.java
+++ hr-worker/src/main/java/com/dev/hrworker/controllers/WorkerController.java
@@ -37,6 +37,13 @@ public class WorkerController {
 
     @GetMapping(value = "/{id}")
     public ResponseEntity<Worker> findById(@PathVariable Long id) {
+        /*
+		try {
+			Thread.sleep(3000L);
+		} catch (InterruptedException e) {
+			e.printStackTrace();
+		}
+		*/
         logger.info("PORT = " + env.getProperty("local.server.port"));
         Optional<Worker> obj = workerService.findById(id);
         if(!obj.isPresent()) {
