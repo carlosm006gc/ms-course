587c524ee329439cbeb31ebd6c4afd2ec28078d6
carlosm006gc <104165201+carlosm006gc@users.noreply.github.com>
2023-02-14 13:56:38 -0300

    Zuul authorization/update

--- hr-api-gateway-zuul/src/main/java/com/dev/hrapigatewayzuul/config/AppConfig.java
+++ hr-api-gateway-zuul/src/main/java/com/dev/hrapigatewayzuul/config/AppConfig.java
@@ -1,4 +1,4 @@
-package config;
+package com.dev.hrapigatewayzuul.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -6,7 +6,6 @@ import org.springframework.security.oauth2.provider.token.store.JwtAccessTokenCo
 import org.springframework.security.oauth2.provider.token.store.JwtTokenStore;
 
 @Configuration
-
 public class AppConfig {
     @Bean
     public JwtAccessTokenConverter accessTokenConverter() {
--- hr-api-gateway-zuul/src/main/java/com/dev/hrapigatewayzuul/config/ResourceServerConfig.java
+++ hr-api-gateway-zuul/src/main/java/com/dev/hrapigatewayzuul/config/ResourceServerConfig.java
@@ -1,4 +1,4 @@
-package config;
+package com.dev.hrapigatewayzuul.config;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
